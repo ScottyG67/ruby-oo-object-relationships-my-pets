81a8e757f232e7ec9a55653fa373c5a895b14c7c
github username <scott.gloyna01@gmail.com>
2021-01-15 14:40:03 -0600

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,37 @@
 class Cat
   # code goes here
+
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @species = "cat"
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def get_fed
+    @mood="happy"
+  end
+
 end
